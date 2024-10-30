ff897af1dfbeaa4140c7379f3b6fdb050a534e07
Igor Batista <igorengbatista@gmail.com>
2024-10-29 23:47:36 -0300

    fim da atividade

--- Atividade 2/Estradas.py
+++ Atividade 2/Estradas.py
@@ -1,96 +1,89 @@
-class min_heap:
+class MinHeap:
     def __init__(self):
         self.heap = []
         self.pos = {}
-    
-    def empty(self):
-        return len(self.heap) == 0
-    
-    def insert(self, v, d):
-        self.heap.append((v, d))
-        self.pos[v] = len(self.heap) - 1
-        self.up(len(self.heap) - 1)
-    
-    def up(self, i):
-        while i > 0:
-            pai = (i - 1) // 2
-            if self.heap[pai][1] > self.heap[i][1]:
-                self.heap[pai], self.heap[i] = self.heap[i], self.heap[pai]
-                self.pos[self.heap[pai][0]] = pai
-                self.pos[self.heap[i][0]] = i
-                i = pai
-            else:
-                break
-    
-    def down(self, i):
-        while 2 * i + 1 < len(self.heap):
-            filho = 2 * i + 1
-            if filho + 1 < len(self.heap) and self.heap[filho + 1][1] < self.heap[filho][1]:
-                filho += 1
-            if self.heap[i][1] > self.heap[filho][1]:
-                self.heap[i], self.heap[filho] = self.heap[filho], self.heap[i]
-                self.pos[self.heap[i][0]] = i
-                self.pos[self.heap[filho][0]] = filho
-                i = filho
-            else:
-                break
-    
-    def decrease_key(self, v, d):
-        i = self.pos[v]
-        self.heap[i] = (v, d)
-        self.up(i)
-    
-    def increase_key(self, v, d):
-        i = self.pos[v]
-        self.heap[i] = (v, d)
-        self.down(i)
+
+    def insert(self, key, value):
+        self.heap.append((key, value))
+        self.pos[key] = len(self.heap) - 1
+        self._bubble_up(len(self.heap) - 1)
 
     def extract_min(self):
-        if self.empty():
+        if not self.heap:
             return None
-        v, d = self.heap[0]
-        self.heap[0] = self.heap[-1]
-        self.pos[self.heap[0][0]] = 0
-        self.heap.pop()
-        self.down(0)
-        return v, d
+        root = self.heap[0]
+        last = self.heap.pop()
+        if self.heap:
+            self.heap[0] = last
+            self.pos[last[0]] = 0
+            self._bubble_down(0)
+        del self.pos[root[0]]
+        return root
 
+    def decrease_key(self, key, new_value):
+        i = self.pos[key]
+        self.heap[i] = (key, new_value)
+        self._bubble_up(i)
 
-def AGM_Prim(G, w, r):
+    def _bubble_up(self, i):
+        parent = (i - 1) // 2
+        while i > 0 and self.heap[i][1] < self.heap[parent][1]:
+            self._swap(i, parent)
+            i = parent
+            parent = (i - 1) // 2
+
+    def _bubble_down(self, i):
+        smallest = i
+        left = 2 * i + 1
+        right = 2 * i + 2
+        if left < len(self.heap) and self.heap[left][1] < self.heap[smallest][1]:
+            smallest = left
+        if right < len(self.heap) and self.heap[right][1] < self.heap[smallest][1]:
+            smallest = right
+        if smallest != i:
+            self._swap(i, smallest)
+            self._bubble_down(smallest)
+
+    def _swap(self, i, j):
+        self.pos[self.heap[i][0]], self.pos[self.heap[j][0]] = self.pos[self.heap[j][0]], self.pos[self.heap[i][0]]
+        self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
+
+    def is_empty(self):
+        return len(self.heap) == 0
+
+def AGM_Prim(G, r):
     n = len(G)
-    d = [float("inf") for _ in range(n)]
-    pai = [-1 for _ in range(n)]
+    d = [float("inf")] * n
+    pai = [-1] * n
     d[r] = 0
-    Q = set(range(n))
-    while Q:
-        u = min(Q, key=lambda x: d[x])
-        Q.remove(u)
-        for v in range(n):
-            if v in Q and w[u][v] < d[v]:
+    min_heap = MinHeap()
+    for v in range(n):
+        min_heap.insert(v, d[v])
+    while not min_heap.is_empty():
+        u, du = min_heap.extract_min()
+        for v, w in G[u]:
+            if v in min_heap.pos and w < d[v]:
+                d[v] = w
                 pai[v] = u
-                d[v] = w[u][v]
+                min_heap.decrease_key(v, w)
     return d, pai
+
 
 rodando = True
 m, n = input().split()
 m, n = int(m), int(n)
 economia = []
 while rodando:
-    # Cria a matriz de distâncias
-    malha = [[float("inf") for _ in range(m)] for _ in range(m)]
-    for _ in range(n):
-        x, y, dist = input().split()
-        x, y, dist = int(x), int(y), int(dist)
-        malha[x][y] = dist
-        malha[y][x] = dist
-    # Calcula a arvore de caminhos mínimos
-    d, pais = AGM_Prim(malha, malha, 0)
-    # Calcula o custo total inicial
+    # Cria a lista de adjacência
+    malha = [[] for _ in range(m)]
     custo_total_ini = 0
-    for i in range(m):
-        for j in range(i + 1, m):
-            if malha[i][j] != float("inf"):
-                custo_total_ini += malha[i][j]
+    for _ in range(n):
+        x, y, dist = map(int, input().split())
+        malha[x].append((y, dist))
+        malha[y].append((x, dist))
+        custo_total_ini += dist
+    # Calcula a arvore de caminhos mínimos
+    d, pais = AGM_Prim(malha, 0)
     # Calcula o custo total final
     custo_total_fim = 0
     for i in range(m):
