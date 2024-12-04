b3a4f66c1194ad3a1c0f90a8d0efe700a24032ef
Igor Batista <igorengbatista@gmail.com>
2024-12-04 19:31:34 -0300

    limpeza de codigo efetuada

--- Projeto/crypto.py
+++ Projeto/crypto.py
@@ -10,18 +10,12 @@ def reduce_3sat_to_cryptarithm(cnf_formula):
 
     Returns:
         dict: Um dicionário representando o cryptarithm, com:
-            - "columns": Colunas do cryptarithm (list of tuples).
-            - "letters": Conjunto de letras usadas.
+            - "linhas": Uma lista de 3 listas, onde cada lista representa uma linha da instância.
             - "base": A base mínima necessária.
     """
-    letters = set()
-    columns = []
-    lines = [[], [], []]
+    linhas = [[], [], []]
     n_vars = max(var for clause in cnf_formula for var, _ in clause)
     base = 3072 * (n_vars ** 3)
-
-    # Definir letras de controle 0 e 1
-    letters.update(['0', '1'])
 
     # Adicionar colunas para variáveis e seus complementos
 
@@ -29,42 +23,29 @@ def reduce_3sat_to_cryptarithm(cnf_formula):
     for i, clause in enumerate(cnf_formula):
         clause_vars = [f'v{var}' if not negated else f'v{var}\''
                        for var, negated in clause]
-        clause_columns = [
-            (f'a{i}', clause_vars[0], f'b{i}'),
-            (f'c{i}', clause_vars[1], f'd{i}'),
-            (f'e{i}', clause_vars[2], f'f{i}')
-        ]
-        columns.extend(clause_columns)
-        letters.update([f'a{i}', f'b{i}', f'c{i}', f'd{i}', f'e{i}', f'f{i}'])
         soma = f'u{clause_vars[0][1:]}{clause_vars[1][1:]}'
-
         # Adicionar linhas para cada cláusula
-        lines[0].extend([soma, '0', f'{clause_vars[0]}', '0', '1', f'r{i}', '0', f'g{i}', f'w{i}', '0', f'f{i}', '0'])
-        lines[1].extend([f'{clause_vars[2]}', '0', f'{clause_vars[1]}', '0', f'h{i}', f'r{i}', '0', f'g{i}', f'w{i}', '0', f'f{i}', '0'])
-        lines[2].extend([f't{i}', '0', soma, '0', f't{i}', f's{i}', '0', f'h{i}', f'x{i}', '0', f'g{i}', '0'])
+        linhas[0].extend([soma, '0', f'{clause_vars[0]}', '0', '1', f'r{i}', '0', f'g{i}', f'w{i}', '0', f'f{i}', '0'])
+        linhas[1].extend([f'{clause_vars[2]}', '0', f'{clause_vars[1]}', '0', f'h{i}', f'r{i}', '0', f'g{i}', f'w{i}', '0', f'f{i}', '0'])
+        linhas[2].extend([f't{i}', '0', soma, '0', f't{i}', f's{i}', '0', f'h{i}', f'x{i}', '0', f'g{i}', '0'])
 
     #Agora, para cada variável, adicionaremos as colunas correspondentes
     for var in range(1, n_vars + 1):
         v = f'V{var}'
         not_v = f'V{var}\''
-        letters.update([v, not_v])
-        # # Adicionar colunas conforme o padrão descrito no artigo
-        lines[0].extend([f'd{var}', '0', '1', f'y{var}', '0', f'c{var}', f'y{var}', '0', f'b{var}', f'y{var}', '0', f'a{var}', '0'])
-        lines[1].extend([f'e{var}', '0', f'd{var}', f'y{var}', '0', f'c{var}', f'y{var}', '0', f'b{var}', f'y{var}', '0', f'a{var}', '0'])
-        lines[2].extend([not_v, '0', f'e{var}', f'z{var}', '0', f'd{var}', f'z{var}', '0', v, f'z{var}', '0', f'b{var}', '0'])
+        # Adicionar colunas conforme o padrão descrito no artigo
+        linhas[0].extend([f'd{var}', '0', '1', f'y{var}', '0', f'c{var}', f'y{var}', '0', f'b{var}', f'y{var}', '0', f'a{var}', '0'])
+        linhas[1].extend([f'e{var}', '0', f'd{var}', f'y{var}', '0', f'c{var}', f'y{var}', '0', f'b{var}', f'y{var}', '0', f'a{var}', '0'])
+        linhas[2].extend([not_v, '0', f'e{var}', f'z{var}', '0', f'd{var}', f'z{var}', '0', v, f'z{var}', '0', f'b{var}', '0'])
 
     #Por fim, adicionamos as 3 colunas da direita, para controle
-    lines[0].extend(['0', 'p', '0'])
-    lines[1].extend(['0', 'p', '0'])
-    lines[2].extend(['1', 'q', '0'])
+    linhas[0].extend(['0', 'p', '0'])
+    linhas[1].extend(['0', 'p', '0'])
+    linhas[2].extend(['1', 'q', '0'])
 
-    print(f"Lines: {len(lines[0])}")
-    for line in lines:
-        print(" ".join(f"{term:>4}" for term in line))
 
     return {
-        "columns": columns,
-        "letters": letters,
+        "linhas": linhas,
         "base": base
     }
 
@@ -74,9 +55,13 @@ cnf_formula = [
     [(1, False), (2, True), (3, False)],  # x1 OR NOT x2 OR x3
     [(1, True), (2, False), (3, False)]  # NOT x1 OR x2 OR x3
 ]
-
 cryptarithm_instance = reduce_3sat_to_cryptarithm(cnf_formula)
-print("Cryptarithm Instance:")
-print("Columns:", cryptarithm_instance["columns"])
-print("Letters:", cryptarithm_instance["letters"])
+
+print("Instância do 3-CNF: ", cnf_formula)
+
+print("Instância do Cryptarithm:")
+print("Linhas: ")
+for linha in cryptarithm_instance["linhas"]:
+    print(" ".join(f"{term:>4}" for term in linha))
+print("Número de colunas:", len(cryptarithm_instance["linhas"][0]))
 print("Base:", cryptarithm_instance["base"])
